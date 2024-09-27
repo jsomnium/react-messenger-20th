8cd88155c43a89a2ffb10d4afd19d55e763ce9df
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-09-27 19:34:25 +0900

    feat: 기본 화면 설정, 채팅방 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ChatRoom from '../src/pages/chatRoom';
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ChatRoom />} /> 
+        {/* 기본 경로: ChatRoom */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/chatRoom.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatRoom: React.FC = () => {
+  return (
+    <div>
+      <h1>채팅방</h1>
+      <p>내용 표시</p>
+    </div>
+  );
+};
+
+export default ChatRoom;
